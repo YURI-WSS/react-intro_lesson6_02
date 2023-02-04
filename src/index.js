b1c1ff01eac3d10380023cccbb671b33b26c29de
YURI-WSS <119521018+YURI-WSS@users.noreply.github.com>
2023-02-04 23:54:06 +0900

    40. 注意事項（Reactのバージョン）

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
+// 書き換え-40. 注意事項（Reactのバージョン）
 import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+import ReactDom from "react-dom";
 
 import App from "./App";
 
-const rootElement = document.getElementById("root");
-const root = createRoot(rootElement);
-
-root.render(
+ReactDom.render(
   <StrictMode>
     <App />
-  </StrictMode>
+  </StrictMode>,
+  document.getElementById("root")
 );

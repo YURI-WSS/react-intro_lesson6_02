a1d7dbcbf6272bd57f4207e5b74992217142b7b9
YURI-WSS <119521018+YURI-WSS@users.noreply.github.com>
2023-02-04 23:58:08 +0900

    41. 準備

--- src/index.js
+++ src/index.js
@@ -4,9 +4,10 @@ import ReactDom from "react-dom";
 
 import App from "./App";
 
+const rootElement = document.getElementById("root");
 ReactDom.render(
   <StrictMode>
     <App />
   </StrictMode>,
-  document.getElementById("root")
+  rootElement
 );

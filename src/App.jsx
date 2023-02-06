3ffd5f4046dc01bfe384b26df87a24564819cd1b
YURI-WSS <119521018+YURI-WSS@users.noreply.github.com>
2023-02-06 23:51:56 +0900

    46. Propsを知る

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,19 @@
 import React from "react";
 import ReactDom from "react-dom";
+import ColorfulMessage from "./components/ColofulMwssage";
 
 const App = () => {
   const onClickButton = () => alert();
-  const contentStyle = {
-    color: "blue",
+  const contentLadyStyle = {
+    color: "pink",
     fontSize: "18px"
   };
 
   return (
     <>
       <h1 style={{ color: "red" }}>こんにちは！</h1>
-      <p style={contentStyle}>お元気ですか？</p>
+      <ColorfulMessage color="blue">お元気ですか？</ColorfulMessage>
+      <ColorfulMessage color="pink">元気です</ColorfulMessage>
       <button onClick={onClickButton}>ボタン</button>
     </>
   );
